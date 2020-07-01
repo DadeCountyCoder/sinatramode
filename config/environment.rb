9d30e5ee115621c27c7c1e10ec60102eae081183
MyMac <mymac@MacBook-Pro.local>
2020-07-01 02:50:01 -0400

    envi

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ Bundler.require
 
 require_relative '../app/models/user'
 require_relative '../app/controllers/application_controller'
-require_relative '../app/controllers/comic_book_controller'
+require_relative '../app/controllers/book_controller'
 require_all 'app'
