e7cd2722e8496806dd95359bc34beccc9826ae06
MyMac <mymac@MacBook-Pro.local>
2020-07-01 02:44:12 -0400

    env

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,8 @@
-ENV['SINATRA_ENV'] ||= "development"
-
 require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
+Bundler.require
+#require 'sinatra/flash'
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
-
-require './app/controllers/application_controller'
+require_relative '../app/models/user'
+require_relative '../app/controllers/application_controller'
+require_relative '../app/controllers/comic_book_controller'
 require_all 'app'
